25d64f060ec6225e051095a7dc5b2c8ee018439d
Vino <sky0master@163.com>
2025-03-01 16:38:16 +0800

    Update FlipWithMovement.cs

--- YummyUtils/Components/FlipWithMovement.cs
+++ YummyUtils/Components/FlipWithMovement.cs
@@ -1,30 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Yummy{
 public class FlipWithMovement : MonoBehaviour
 {
-    private Vector3 lastPos;
+   public bool autoFlipX = true;
+    public bool isSetAnimator = true;
+    public string verticalMoveKey = "Vertical";
+    public string horizontalMoveKey = "Horizontal";
+    public string isMovingKey = "IsMoving";
+    public float sens = 0.01f;
+    private Vector3 _lastPos;
+    private float _moveOffsetX;
+    private float _moveOffsetY;
+    private bool _isMoving;
     void Start()
     {
-        lastPos = transform.position;
+        _lastPos = transform.position;
     }
-
-    // Update is called once per frame
-    void Update()
+    void Flip()
     {
         var scalex = Mathf.Abs(transform.localScale.x);
-        var moveOffset = transform.position.x - lastPos.x;
-        if(moveOffset > 0)
+        if(_moveOffsetX > 0)
         {
             transform.localScale = new Vector3(scalex ,transform.localScale.y,transform.localScale.z);
         }
-        else if (moveOffset < 0) 
+        else if (_moveOffsetX < 0) 
         {
             transform.localScale = new Vector3(-scalex,transform.localScale.y,transform.localScale.z);
         }
-        lastPos = transform.position;
+    }
+    void UpdateAnimator()
+    {
+        var animator = GetComponent<Animator>();
+        if(animator == null)
+        {
+            return;
+        }
+        if(Mathf.Abs(_moveOffsetX) > sens)
+        {
+            animator.SetFloat(horizontalMoveKey,Mathf.Sign(_moveOffsetX));
+        }
+        else {
+            animator.SetFloat(horizontalMoveKey,0);
+        }
+
+        if(Mathf.Abs(_moveOffsetY) > sens)
+        {
+            animator.SetFloat(verticalMoveKey,Mathf.Sign(_moveOffsetY));
+        }
+        else{
+            animator.SetFloat(verticalMoveKey,0);
+        }
+
+        animator.SetBool(isMovingKey , _isMoving);
+
+    }
+    void FixedUpdate()
+    {
+        _moveOffsetX = transform.position.x - _lastPos.x;
+        _moveOffsetY = transform.position.y - _lastPos.y;
+        _lastPos = transform.position;
+    }
+    void Update()
+    {
+        if(new Vector2(_moveOffsetX,_moveOffsetY).magnitude > sens )
+        {
+            _isMoving = true;
+        }
+        else{
+            _isMoving = false;
+        }
+
+        if(autoFlipX)
+        {
+            Flip();
+        }
+        if(isSetAnimator)
+        {
+            UpdateAnimator();
+        }
+        
     }
 }
 }
